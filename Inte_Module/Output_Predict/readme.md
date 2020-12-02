411bb8261d9ec9fb05641e6c4ab3551dd85a8205
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-12-02 14:15:55 +0530

    initial commit

---
+++ Inte_Module/Output_Predict/readme.md
@@ -0,0 +1,7 @@
+# Questions
+
+1. If value is a variable will this ever evaluate to true ? Which kind of value can we use in this statement
+
+```
+    value !== value
+```
---
+++ Inte_Module/Output_Predict/solutions.js
@@ -0,0 +1,7 @@
+/* 1.  The only solution that will result for the statement is NaN ( Not a Number) . Because NaN are not 
+compareable 
+*/
+console.log(NaN !== NaN)
+
+/*----------------------------------------------------------------------------------------------------*/
+
